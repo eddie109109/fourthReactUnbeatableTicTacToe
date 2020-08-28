fd0440a77a22213951e41c0d2917eb98a807b28f
jundic <jundi_chen@sfu.ca>
2020-08-28 05:40:47 -0700

    Updates

---
+++ precache-manifest.dabc6d975cf68d6fb222abf6c4d34e08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3b4795340a941054b4cdbfad43b141",
+    "url": "/fourthReactUnbeatableTicTacToe/index.html"
+  },
+  {
+    "revision": "a9204f5226c65f72b786",
+    "url": "/fourthReactUnbeatableTicTacToe/static/js/2.8ea3bd20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fourthReactUnbeatableTicTacToe/static/js/2.8ea3bd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5807c735faf9217592",
+    "url": "/fourthReactUnbeatableTicTacToe/static/js/main.833c4122.chunk.js"
+  },
+  {
+    "revision": "16d60c2ccee1738ad7d0",
+    "url": "/fourthReactUnbeatableTicTacToe/static/js/runtime-main.0fa10904.js"
+  }
+]);
